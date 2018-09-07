cdbadb9392d80ab025f0f0f2517d6f62e3e95512
EleMint <dylanschmidt0216@gmail.com>
2018-09-07 10:55:55 -0500

    Working Bubble Sort, Made Files For Others

---
+++ BubbleSort/scripts.js
@@ -0,0 +1,49 @@
+var values = [];
+var i = 0;
+var j = 0;
+var counter = 0;
+
+function setup() {
+    createCanvas(500, 400);
+
+    for (let i = 0; i < width; i++) {
+        values[i] = random(height - 50);
+    }
+}
+
+function draw() {
+    background(51);
+    strokeWeight(1);
+    for (let i = 0; i < values.length; i++) {
+        stroke(255);
+        line(i, height, i, height - values[i]);
+    }
+    
+    for (let amt = 0; amt < 30; amt++) {
+        var a = values[j];
+        var b = values[j + 1];
+        if (a > b) {
+            Swap(values, j, j + 1);
+        }
+        if (i < values.length) {
+            j++;
+            if (j >= values.length - i - 1) {
+                j = 0;
+                i++
+            }
+        } else {
+            noLoop();
+            console.log('Finished');
+            break;
+        }
+        counter++;
+        console.log(counter);
+    }
+}
+
+function Swap(arr, a, b) {
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+    return arr;
+}
